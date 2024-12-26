73795c0fbbff341b15c73a9dc2b3b9747cb467c8
KyungHo Kim <hao@smartmind.team>
2024-12-27 08:46:30 +0900

    feat: calc dynamic sleeping time 1. add dynamic sleeping time for reduce resource usage

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from utils.logger import logger
 from utils.settings import settings
 from modules.hooking import slack_hooking
 
+
 def main():
     log_msg = f"Start slack webhook at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
     log_msg += f"\nSlack webhook url: {settings.slack_webhook_url}"
@@ -14,22 +15,35 @@ def main():
 
     while True:
         now = datetime.now()
-        if now.hour == 9:
-            if not check_date.is_working_day(now):
-                logger.info(f"Today is not a working day: {now.strftime('%Y-%m-%d %H:%M:%S')}")
-                time.sleep(60 * 60 * 24)  # 하루 뒤로 이동
-                continue
+        if not check_date.is_working_day(now):
+            logger.info(
+                f"Today is not a working day: {now.strftime('%Y-%m-%d %H:%M:%S')}"
+            )
+            next_working_day = check_date.get_next_working_day(now)
+            next_lunch_time = check_date.get_next_lunch_time(next_working_day)
+            time_diff = (next_lunch_time - now).total_seconds()
+            logger.info(f"Sleeping for {time_diff/60} minutes")
+            time.sleep(time_diff)
+            continue
         if not check_date.is_lunch_hour(now):
-            logger.info(f"Not lunch hour: {now.strftime('%Y-%m-%d %H:%M:%S')}")
-            time.sleep(60 * 60 * 1)  # 1시간 뒤로 이동
+            next_lunch_time = check_date.get_next_lunch_time(now)
+            time_diff = (next_lunch_time - now).total_seconds()
+            logger.info(f"Sleeping for {time_diff/60} minutes")
+            time.sleep(time_diff)
             continue
         if not check_date.is_lunch_minute(now):
-            logger.info(f"Not lunch minute: {now.strftime('%Y-%m-%d %H:%M:%S')}")
-            time.sleep(60)  # 1분 뒤로 이동
+            next_lunch_time = check_date.get_next_lunch_time(now)
+            time_diff = (next_lunch_time - now).total_seconds()
+            logger.info(f"Sleeping for {time_diff/60} minutes")
+            time.sleep(time_diff)
             continue
         logger.debug("It's time to send slack webhook")
         slack_hooking.send_lunch_webhook()
-        time.sleep(60 * 60 * 20)  # 20시간 뒤로 이동
+        next_working_day = check_date.get_next_working_day(now.replace(hour=13))
+        next_lunch_time = check_date.get_next_lunch_time(next_working_day)
+        time_diff = (next_lunch_time - now).total_seconds()
+        logger.info(f"Sleeping for {time_diff/60} minutes")
+        time.sleep(time_diff)
 
 
 if __name__ == "__main__":
--- utils/check_date.py
+++ utils/check_date.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def is_working_day(date: datetime) -> bool:
@@ -10,8 +10,22 @@ def is_lunch_hour(date: datetime) -> bool:
 
 
 def is_lunch_minute(date: datetime) -> bool:
-    return date.minute == 10
+    start_minute = 10
+    end_minute = 30
+    return start_minute <= date.minute <= end_minute
 
 
 def is_lunch_time(date: datetime) -> bool:
     return is_lunch_hour(date) and is_lunch_minute(date)
+
+
+def get_next_working_day(date: datetime) -> datetime:
+    while not is_working_day(date):
+        date += timedelta(days=1)
+    return date
+
+
+def get_next_lunch_time(date: datetime) -> datetime:
+    while not is_lunch_time(date):
+        date += timedelta(minutes=1)
+    return date
